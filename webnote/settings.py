2729a9dfaa8c3bf3d88352b89499d83bbcedf6d3
Kalebe Gouvea <k.gouvea@live.com>
2021-01-09 16:21:48 -0300

    aba sobre finalizada, debug mode desativado

--- webnote/settings.py
+++ webnote/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'zg)qu2pkc%mw_cna#@vhmdv!$gr(e&p%s6$%#88g7n)y3geb#)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
