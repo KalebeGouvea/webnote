099669c33bce949fe56bff4c98feb1a5a0a7cb41
Kalebe Gouvea <k.gouvea@live.com>
2021-04-29 20:23:44 -0300

    settings alterado para producao

--- webnote/settings.py
+++ webnote/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zg)qu2pkc%mw_cna#@vhmdv!$gr(e&p%s6$%#88g7n)y3geb#)'
+#SECRET_KEY = 'zg)qu2pkc%mw_cna#@vhmdv!$gr(e&p%s6$%#88g7n)y3geb#)'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'zg)qu2pkc%mw_cna#@vhmdv!$gr(e&p%s6$%#88g7n)y3geb#)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['*']
 
